ecc63caa1cfbd3947f7d369a398278f815f50cac
HiTman77 <doug@kadabra.co.za>
2022-07-10 17:37:28 +0200

    Update theme.php

--- theme.php
+++ theme.php
@@ -6,12 +6,12 @@
 <meta name="description" content="<?= $Wcms->page('description') ?>">
 <meta charset="utf-8" />
 <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
-<link rel="stylesheet" href="/themes/hyper/assets/css/main.css?v<?php echo(rand(1,10));?><?php echo(rand(1,20));?>" />
-<noscript><link rel="stylesheet" href="/themes/hyper/assets/css/noscript.css" /></noscript>
+<link rel="stylesheet" href="/themes/hype/assets/css/main.css?v<?php echo(rand(1,10));?><?php echo(rand(1,20));?>" />
+<noscript><link rel="stylesheet" href="/themes/hype/assets/css/noscript.css" /></noscript>
 <link rel="shortcut icon" href="/data/files/favicon.png">
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
 <?= $Wcms->css() ?>
-</head>
+</head>  
 <body class="is-preload">
 <?= $Wcms->settings() ?>
 <?= $Wcms->alerts() ?>
@@ -41,13 +41,13 @@
 <?= $Wcms->footer() ?>
 </div>
 </footer>
-<script src="/themes/hyper/assets/js/jquery.min.js"></script>
-<script src="/themes/hyper/assets/js/jquery.scrollex.min.js"></script>
-<script src="/themes/hyper/assets/js/jquery.scrolly.min.js"></script>
-<script src="/themes/hyper/assets/js/browser.min.js"></script>
-<script src="/themes/hyper/assets/js/breakpoints.min.js"></script>
-<script src="/themes/hyper/assets/js/util.js"></script>
-<script src="/themes/hyper/assets/js/main.js"></script>
+<script src="/themes/hype/assets/js/jquery.min.js"></script>
+<script src="/themes/hype/assets/js/jquery.scrollex.min.js"></script>
+<script src="/themes/hype/assets/js/jquery.scrolly.min.js"></script>
+<script src="/themes/hype/assets/js/browser.min.js"></script>
+<script src="/themes/hype/assets/js/breakpoints.min.js"></script>
+<script src="/themes/hype/assets/js/util.js"></script>
+<script src="/themes/hype/assets/js/main.js"></script>
 <?= $Wcms->js() ?>
 </body>
 </html>
